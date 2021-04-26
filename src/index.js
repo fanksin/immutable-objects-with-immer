e7c2f12c201b90bd8280e9bee4f01d5af7d17357
Serhat Aksakal <49079271+fanksin@users.noreply.github.com>
2021-04-26 16:47:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import { produce } from "immer";
 
-let movie = { title: "Lord of the Rings" };
+const movie = { title: "Lord of the Rings" };
 
 function release(movie) {
   return produce(movie, (directorsCut) => {
     directorsCut.isReleased = true;
   });
 }
-let updatedMovie = release(movie);
+const updatedMovie = release(movie);
 console.log(movie);
 console.log(updatedMovie);
 
-let book = { title: "Harry Potter" };
+const book = { title: "Harry Potter" };
 
 function publish(book) {
   return produce(book, (draftBook) => {
     draftBook.isPublished = true;
   });
 }
-let updatedBook = publish(book);
+const updatedBook = publish(book);
 console.log(book);
 console.log(updatedBook);
